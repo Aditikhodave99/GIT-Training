7cff861feff028093fdf7679e2f3f09567a3ed78
Aditikhodave99 <76159997+Aditikhodave99@users.noreply.github.com>
2021-02-24 12:44:38 +0530

    Added a method

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
-import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
-
 class Hello{
+    
+    public static calFactorial(x){}
     public static void main(String args[]){
         System.out.println("HELLO WORLd");
 
